d8b34acb2fa18866b200cb1f3c1e1b9b53a0fe6f
Mysystem <email2boopathi@gmail.com>
2016-08-30 00:12:31 +0530

    My Initial commit for salesdata

---
+++ JavaLearning/src/com/java/learning/Salesdata.java
@@ -0,0 +1,8 @@
+package com.java.learning;
+
+public class Salesdata {
+ public void display()
+ {
+	 System.out.println("SalesClass Data");
+ }
+}
---
+++ JavaLearning/src/com/java/learning/Simpleclass.java
@@ -0,0 +1,15 @@
+package com.java.learning;
+
+public class Simpleclass {
+
+	public static void main(String[] args) {
+		Salesdata data=new Salesdata();
+		displaycreating();
+		data.display();
+		
+	}
+	private static void displaycreating(){
+		System.out.println("Main class");
+	}
+
+}
